87076cc44da1da49488fd6c33f6433815de50a50
botij0 <javier.gonzalez.soldado@gmail.com>
2025-07-28 15:48:59 +0200

    feat: deck init

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,10 +6,24 @@
  */
 
 let deck = [];
+const suites = ['C', 'D', 'H', 'S'];
+const specials = {
+  1: 'A',
+  11: 'J',
+  12: 'Q',
+  13: 'K'
+}
 
 const initDeck = () => {
-
+  for (let i = 1; i <= 13; i++){
+    for (let suite of suites) {
+      const val = specials[i] ? specials[i] : i
+      deck.push(val + suite)
+    }
+  }
+  console.log(deck)
+  deck = _.shuffle(deck)
+  console.log(deck)
 }
 
 initDeck();
-
