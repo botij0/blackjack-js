630475b88cba02cbcdc303075d9a22f1c28aba65
botij0 <javier.gonzalez.soldado@gmail.com>
2025-07-28 19:01:58 +0200

    feat: advances

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,9 +21,23 @@ const initDeck = () => {
       deck.push(val + suite)
     }
   }
-  console.log(deck)
   deck = _.shuffle(deck)
-  console.log(deck)
 }
 
 initDeck();
+
+
+const dealCard = () => {
+  if (deck.length === 0) {
+    throw 'No remain cards in deck'
+  }
+  return deck.pop()
+}
+
+const cardValue = (card) => {
+  const num = card.substring(0, card.length - 1);
+  return !isNaN(num) ? Number(num) : num === 'A' ? 11 : 10;
+  // Number(x) is equivalent to x * 1
+}
+
+console.log(cardValue('7D'))
